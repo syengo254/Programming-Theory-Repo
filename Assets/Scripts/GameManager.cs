f49cb250ef7a0f32d9fae3e518b7fb472f4cf159
David Syengo <david.syengo019@gmail.com>
2025-02-07 17:03:43 +0300

    fix: encapsulate game state error

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -25,7 +25,7 @@ public class GameManager : MonoBehaviour
         set{
             if(value == null)
             {
-
+                Debug.LogError("Game state cannot be set to null!");
             }
             else
             {
