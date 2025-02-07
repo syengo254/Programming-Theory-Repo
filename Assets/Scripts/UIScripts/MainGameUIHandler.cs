bbb6ce405ae2bf9a1e0b944c8386d2322579c7c5
David Syengo <david.syengo019@gmail.com>
2025-02-07 13:51:53 +0300

    feat: added game over canvas and UI

--- Assets/Scripts/UIScripts/MainGameUIHandler.cs
+++ Assets/Scripts/UIScripts/MainGameUIHandler.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainGameUIHandler : MonoBehaviour
 {
     public void HandleMenuButton()
     {
-        GameManager.Instance.ShowMenu();
+        GameManager.Instance?.ShowMenu();
+    }
+
+    public void HandleRestartGameButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     } 
 }
