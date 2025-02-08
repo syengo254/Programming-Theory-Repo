e17655424b28ffcf8981830321168a9c9320b1ef
David Syengo <david.syengo019@gmail.com>
2025-02-08 15:55:13 +0300

    fix: removed typos and unused namespaces

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
-using UnityEditor.Build;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
